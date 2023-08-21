1c5cf226391c73a3058f2f758c21670029e6766a
LingFenglong <2808021998@qq.com>
2023-08-21 22:07:09 +0800

    all

--- common/spring-security/src/main/java/com/lingfenglong/security/config/WebSecurityConfig.java
+++ common/spring-security/src/main/java/com/lingfenglong/security/config/WebSecurityConfig.java
@@ -1,84 +1,72 @@
 package com.lingfenglong.security.config;
 
-import com.lingfenglong.security.custom.CustomMd5PasswordEncoder;
 import com.lingfenglong.security.custom.PasswordEncoderImpl;
 import com.lingfenglong.security.filter.TokenAuthenticationFilter;
 import com.lingfenglong.security.filter.TokenLoginFilter;
+import org.apache.catalina.Manager;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.ProviderManager;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.WebSecurityConfigurer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-
 
 @Configuration
 @EnableWebSecurity
 public class WebSecurityConfig implements WebSecurityConfigurer<WebSecurity> {
-    private final UserDetailsService userDetailsService;
-    private final PasswordEncoderImpl passwordEncoder;
+//    private final UserDetailsService userDetailsService;
+//    private final PasswordEncoderImpl passwordEncoder;
+//
+//    @Autowired
+//    public WebSecurityConfig(UserDetailsService userDetailsService, PasswordEncoderImpl passwordEncoder) {
+//        this.userDetailsService = userDetailsService;
+//        this.passwordEncoder = passwordEncoder;
+//    }
 
-    @Autowired
-    public WebSecurityConfig(UserDetailsService userDetailsService, PasswordEncoderImpl passwordEncoder) {
-        this.userDetailsService = userDetailsService;
-        this.passwordEncoder = passwordEncoder;
+    @Bean
+    public AuthenticationManager authenticationManager() {
+        return new ProviderManager();
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http, AuthenticationManager authenticationManager) throws Exception {
+        http
+                .authenticationManager(authenticationManager)
+                .csrf(AbstractHttpConfigurer::disable)  // 关闭csrf跨站请求伪造
+                .cors(Customizer.withDefaults())        // 开启跨域以便前端调用接口
+                .authorizeHttpRequests(authorize -> authorize
+                        .requestMatchers("/admin/system/index/login").permitAll()   // 指定某些接口不需要通过验证即可访问
+                        .anyRequest().authenticated()   // 其它所有接口需要认证才能访问
+                )
+                // TokenAuthenticationFilter放到UsernamePasswordAuthenticationFilter的前面
+                // 这样做就是为了除了登录的时候去查询数据库外，其他时候都用token进行认证
+                .addFilterBefore(new TokenAuthenticationFilter(), UsernamePasswordAuthenticationFilter.class)
+                .addFilter(new TokenLoginFilter(new ProviderManager()))
+                .sessionManagement(manager -> manager.sessionCreationPolicy(SessionCreationPolicy.STATELESS));   // 禁用session
+
+        return http.build();
     }
 
     @Override
     public void init(WebSecurity builder) throws Exception {
-    }
 
-
-
-    @Bean
-    protected AuthenticationManager authenticationManager() throws Exception {
-        return super.authenticationManager();
     }
 
     @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        // 这是配置的关键，决定哪些接口开启防护，哪些接口绕过防护
-        http
-                //关闭csrf跨站请求伪造
-                .csrf().disable()
-                // 开启跨域以便前端调用接口
-                .cors().and()
-                .authorizeRequests()
-                // 指定某些接口不需要通过验证即可访问。登陆接口肯定是不需要认证的
-                .antMatchers("/admin/system/index/login").permitAll()
-                // 这里意思是其它所有接口需要认证才能访问
-                .anyRequest().authenticated()
-                .and()
-                //TokenAuthenticationFilter放到UsernamePasswordAuthenticationFilter的前面，这样做就是为了除了登录的时候去查询数据库外，其他时候都用token进行认证。
-                .addFilterBefore(new TokenAuthenticationFilter(), UsernamePasswordAuthenticationFilter.class)
-                .addFilter(new TokenLoginFilter(authenticationManager()));
-
-        //禁用session
-        http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
-    }
-
-    @Override
-    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        // 指定UserDetailService和加密器
-        auth.userDetailsService(userDetailsService)
-                .passwordEncoder(customMd5PasswordEncoder);
-    }
-
-    /**
-     * 配置哪些请求不拦截
-     * 排除swagger相关请求
-     * @param web
-     * @throws Exception
-     */
-    @Override
-    public void configure(WebSecurity web) throws Exception {
-        web.ignoring().antMatchers("/favicon.ico","/swagger-resources/**", "/webjars/**", "/v2/**", "/swagger-ui.html/**", "/doc.html");
+    public void configure(WebSecurity builder) throws Exception {
+        builder
+                .ignoring().requestMatchers("/favicon.ico","/swagger-resources/**", "/webjars/**",
+                        "/v2/**", "/swagger-ui.html/**", "/doc.html");
     }
 }
